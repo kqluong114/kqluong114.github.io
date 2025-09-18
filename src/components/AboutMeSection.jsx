95b681c2befe5f66365efd997ccef1235e45b5f2
kqluong <kqluong@umich.edu>
2025-09-17 23:57:30 -0400

    More small tweaks to about me

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -1,25 +1,29 @@
 const AboutMeSection = () => {
   return (
-    <div className="bg-(--color-shade-400) p-6 rounded-md flex flex-col gap-4">
-      <span className="text-3xl font-bold">About Me</span>
-      <span className="text-xl font-semibold">Software Engineer & Lifelong Learner</span>
-      <p>
-        I'm a passionate software engineer with a strong interest in building
-        creative and interactive web applications. I enjoy exploring new technologies,
-        improving my skills in full-stack development, and solving challenging problems.
-      </p>
-      <p>
-        Outside of coding, I’m a dedicated language learner and an avid gamer.
-        I love exploring different cultures through languages and immersing myself
-        in rich, story-driven games. Balancing hobbies with learning keeps me motivated
-        and constantly growing.
-      </p>
-      <p>
-        My current focus is on developing scalable web applications with modern
-        technologies like React, Tailwind CSS, and Node.js, while expanding my knowledge
-        of game development and full-stack systems. I’m always looking for opportunities
-        to collaborate, learn, and contribute to meaningful projects.
-      </p>
+    <div className="bg-(--color-shade-400) p-2 py-6 rounded-md flex flex-col gap-4">
+      <div className="flex flex-col gap-1">
+        <span className="text-3xl font-bold">About Me</span>
+        <span className="text-xl font-semibold">Software Engineer & Lifelong Learner</span>
+      </div>
+      <div className="flex flex-col leading-relaxed text-left gap-6 p-2 sm:px-8">
+        <p>
+          I'm a passionate software engineer with a strong interest in building
+          creative and interactive web applications. I enjoy exploring new technologies,
+          improving my skills in full-stack development, and solving challenging problems.
+        </p>
+        <p>
+          Outside of coding, I’m a dedicated language learner and an avid gamer.
+          I love exploring different cultures through languages and immersing myself
+          in rich, story-driven games. Balancing hobbies with learning keeps me motivated
+          and constantly growing.
+        </p>
+        <p>
+          My current focus is on developing scalable web applications with modern
+          technologies like React, Tailwind CSS, and Node.js, while expanding my knowledge
+          of game development and full-stack systems. I’m always looking for opportunities
+          to collaborate, learn, and contribute to meaningful projects.
+        </p>
+      </div>
     </div>
   );
 };
