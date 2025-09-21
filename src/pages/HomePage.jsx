a4570426c5326b6d5e7b4240ce8d1d56c4f1a43c
kqluong <kqluong@umich.edu>
2025-09-20 23:29:01 -0400

    Small changes to json files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,22 +2,15 @@ import ProfileSection from "../components/ProfileSection";
 import ProjectsSection from "../components/ProjectsSection";
 import ContactSection from "../components/ContactSection";
 import AboutMeSection from "../components/AboutMeSection";
-import { useRef } from "react";
 
 function Home() {
-  const profileRef = useRef(null);
-  const aboutMeRef = useRef(null);
-  const projectsRef = useRef(null);
-  const contactsRef = useRef(null);
-
   return (
     <div className="flex flex-col gap-3">
-      <ProfileSection className="scroll-mt-16" ref={profileRef} />
-      <AboutMeSection className="scroll-mt-16" ref={aboutMeRef} />
-      <ProjectsSection className="scroll-mt-16" ref={projectsRef} />
-      <ContactSection className="scroll-mt-16" ref={contactsRef} />
+      <ProfileSection />
+      <AboutMeSection />
+      <ProjectsSection />
+      <ContactSection />
     </div>
   );
 }
-
 export default Home;
